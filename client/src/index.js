5be4d10a9ca67763d839a967851b392577c5bd0a
MohamedArif20091999 <mohamedarifcbe@gmail.com>
2021-07-14 11:34:57 +0530

    feat: Added redux, redux-thunk to manage state

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
+import reducers from "./reducers";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
